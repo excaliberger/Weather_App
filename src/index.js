685e11be1c9d63a9a7a66762b7879232beba7dbc
Emily Berger <excaliberger@gmail.com>
2022-12-12 12:50:58 -0500

    update

--- src/index.js
+++ src/index.js
@@ -1,32 +1,119 @@
 import Weather from "./weather-class.js";
-
+import SavedCitiesList from "./weather-list-class";
 
 
 const apiKey = "e13002eddd35b99554bbc7eb18a634aa";
-const cityListLimit = 5;
+const cityListLimit = 4;
 
+const cityName = document.getElementById("cityName");
 const citySearchBar = document.getElementById("cityInput");
 const citySearchButton = document.getElementById("citySearchButton");
-const sevenDayForecastContainer = document.getElementById("sevenDayForecastContainer");
 const temperatureDisplay = document.getElementById("temperatureDisplay");
-const chanceOfRain = document.getElementById("chanceOfRain")
-const cityListDisplayUl = document.createElement("ul");
+const cloudCoverage = document.getElementById("cloudCoverage")
+const humidity = document.getElementById("humidity");
+const windSpeed = document.getElementById("windSpeed");
+const sunrise = document.getElementById("sunrise");
+const sunset = document.getElementById("sunset");
+const saveButton = document.getElementById("saveButton");
+let listOfSavedCities = new SavedCitiesList([]);
+let cityId = 0;
 
-citySearchButton.onclick = () => {
+citySearchButton.onclick = (click) => {
+    click.preventDefault();
     temperatureDisplay.textContent = ""
-    chanceOfRain.textContent = ""
+    cloudCoverage.textContent = ""
+
     fetch(
         `http://api.openweathermap.org/geo/1.0/direct?q==${citySearchBar.value}&limit=${cityListLimit}&appid=${apiKey}`,
         { mode: "cors" }
     )
     .then((response) => response.json())
     .then((res) => {
-        console.log("res test");
-        temperatureDisplay.textContent = weather.temperature;
-        chanceOfRain.textContent = `${weather.chanceOfRain} chance of rain today`;
+        giveWeatherOptions(res);
     })
     .catch ((err) => {
         console.error(err);
-        feedbackP.textContent = err.message;
     })
 }
+
+function giveWeatherOptions(cityArray) {
+    const weatherOption = document.getElementById("searchOptionsContainer");
+    weatherOption.innerHTML = "";
+    cityArray.forEach((city) => {
+        let childElement = document.createElement("div");
+        childElement.textContent = `${city.name}, ${city.state}`
+        childElement.className = "individualCityOptions";
+        childElement.addEventListener("click", (click) => {
+            click.preventDefault();
+            let latitude = city.lat;
+            let longitude = city.lon;
+            fetch (
+                `https://api.openweathermap.org/data/2.5/weather?lat=${latitude}&lon=${longitude}&appid=${apiKey}`,
+                { mode: "cors" }
+            ) .then ((response) => response.json())
+            .then((res) => {
+
+                let myWeatherAppArgs = {
+                    city: city.name,
+                    state: city.state,
+                    temperature: res.main.temp,
+                    cloudCoverage: res.weather[0].main,
+                    humidity: res.main.humidity,
+                    wind: res.wind.speed,
+                    sunrise: res.sys.sunrise,
+                    sunset: res.sys.sunset,
+                    id: cityId++,
+                }
+                
+                let newWeatherObject = new Weather (myWeatherAppArgs);
+                clickToPopulate(newWeatherObject);
+
+            })
+        
+        });
+        weatherOption.appendChild(childElement);
+        
+    })
+} 
+
+function clickToPopulate(weatherObject) {
+    let activeCity;
+    cityName.innerHTML = `${weatherObject.city}, ${weatherObject.state}`;
+    temperatureDisplay.innerHTML = `${weatherObject.temperatureInFarenheit()} <sup>o</sup>F`;
+    cloudCoverage.innerHTML = `${weatherObject.cloudCoverage}`;
+    humidity.innerHTML = `${weatherObject.humidity}%`;
+    windSpeed.innerHTML = `${weatherObject.wind}mph`;
+    sunrise.innerHTML = `${weatherObject.timeInRegularTimeNotation(weatherObject.sunrise)}`;
+    sunset.innerHTML = `${weatherObject.timeInRegularTimeNotation(weatherObject.sunset)}`;
+    activeCity = weatherObject;
+    if (activeCity.cloudCoverage == "Clear") {
+        document.body.style.backgroundImage = "url('imgs/clear-sky.jpg')";
+        return;
+    } else if (activeCity.cloudCoverage == "Clouds") {
+        document.body.style.backgroundImage = "url('imgs/cloudy-sky.jpg')";
+        return;
+    } else if (activeCity.cloudCoverage == "Fog") {
+        document.body.style.backgroundImage = "url('imgs/fog.jpg')";
+        return;
+    } else if (activeCity.cloudCoverage == "Rain") {
+        document.body.style.backgroundImage = "url('imgs/rainclouds.jpg')";
+        return;
+    } else if (activeCity.cloudCoverage == "Thunderstorm") {
+        document.body.style.backgroundImage = "url('imgs/thunderstorm.jpg')";
+        return;
+    } else if (activeCity.cloudCoverage == "Mist") {
+        document.body.style.backgroundImage = "url('imgs/mist.jpg')";
+        return;
+    } else if (activeCity.cloudCoverage == "Drizzle") {
+        document.body.style.backgroundImage = "url('imgs/drizzle.jpg')";
+        return;
+    }
+}
+
+saveButton.addEventListener("click", (click) => {
+    click.preventDefault();
+    listOfSavedCities.addCity();
+});
+
+
+export default clickToPopulate();
--- src/weather-class.js
+++ src/weather-class.js
@@ -1,9 +1,33 @@
 class Weather {
     constructor (weatherAppArgs) {
         this.city = weatherAppArgs.city,
+        this.state = weatherAppArgs.state,
         this.temperature = weatherAppArgs.temperature,
-        this.chanceOfRain = weatherAppArgs.chanceOfRain
+        this.cloudCoverage = weatherAppArgs.cloudCoverage,
+        this.humidity = weatherAppArgs.humidity,
+        this.wind = weatherAppArgs.wind,
+        this.sunrise = weatherAppArgs.sunrise,
+        this.sunset = weatherAppArgs.sunset,
+        this.id = weatherAppArgs.id
     }    
+
+    temperatureInCelsius(tempInCelsius) {
+        return tempInCelsius = Math.round(this.temperature - 273.15);
+    }
+
+    temperatureInFarenheit(tempInFarenheit) {
+        return tempInFarenheit = Math.round((this.temperatureInCelsius() * 1.8) + 32);
+    }
+
+    timeInRegularTimeNotation(time) {
+        const unixTimestamp = time * 1000;
+        const date = new Date (unixTimestamp);
+        const hours = date.getHours();
+        const minutes = date.getMinutes();
+        const regularTime = minutes < 10 ? `${hours} : 0${minutes}EST` : `${hours} : ${minutes}EST`;
+        return regularTime;
+    }
+
 }
 
 export default Weather;
